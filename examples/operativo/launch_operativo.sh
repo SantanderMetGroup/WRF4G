93d3ac0312820fc0e014c225ca33a716f06ebe61
chus <chus@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2009-06-02 19:19:21 +0000

    o Improved postprocessor in the 'operativo' example.

--- examples/operativo/launch_operativo.sh
+++ examples/operativo/launch_operativo.sh
@@ -5,9 +5,10 @@ if test -z "$sdate"; then
   echo "Launch date missing"
   exit
 fi
-
+#
+#  Some hard-coded paths to make it portable...
+#
 export PATH="/oceano/gmeteo/WORK/chus/wrf4g/ui/scripts:$PATH"
-
 cd /oceano/gmeteo/WORK/chus/experimentos/operativo
 
 syy=${sdate:0:4}
@@ -19,12 +20,18 @@ read fyy fmm fdd fhh <<<`date '+%Y %m %d %H' -d "${syy}-${smm}-${sdd} ${shh}:00 
 fecha="${syy}-${smm}-${sdd}_${shh}:00:00"
 fechafin="${fyy}-${fmm}-${fdd}_${fhh}:00:00"
 
-cat << EOF > wrf.input
+cat << EOF | cat - wrf.input.in > wrf.input
 experiment_name = "oper${sdate}"
 start_date = "${fecha}"
 end_date = "${fechafin}"
 EOF
 
-cat wrf.input.in >> wrf.input
-
 wrf4g_submitter.sh
+#
+#  Postprocess
+#
+pids=$(awk '{printf ",%s", $1}' pids.oper${sdate})
+pids=${pids:1} # strip the leading comma
+sed -e 's/^\ *sdate\ *=.*$/sdate='${sdate}'/' post_operatorio.sh > postop.sh
+qsub -W depend=afterany:${pids} postop.sh
+rm -f postop.sh
--- examples/operativo/post_operatorio.sh
+++ examples/operativo/post_operatorio.sh
@@ -26,23 +26,43 @@ yfirst = 35.5
 yinc = 0.15
 EOF
 
+cat << EOF > grid05.cdo
+gridtype = lonlat
+xsize = 174
+ysize = 43
+xfirst = -9.7
+xinc = 0.05
+yfirst = 41.95
+yinc = 0.05
+EOF
+
 function get_nc_timerecords(){
   ncdump -h $1 | grep UNLIMITED | tr -d -c '0-9'
 }
 
-for rea in /oceano/gmeteo/WORK/MDM.UC/WRF/experiments/oper${sdate}/*; do
-  echo "Processing: $rea"
-  ${wrfncpost} ${rea}'/output/wrfout_d01*.nc' tmp.nc "T2,U10,V10,RAINC,RAINNC"
-  cdo remapbil,grid15.cdo tmp.nc tmp2.nc
+
+function procesa(){
+  rea=$1
+  dom=$2
+  grid=$3
+  ${wrfncpost} ${rea}/output/wrfout_${dom}'*'.nc tmp.nc "T2,U10ER,V10ER,RAINC,RAINNC,SWDOWN"
+  cdo remapbil,${grid} tmp.nc tmp2.nc
   ntimes=$(get_nc_timerecords tmp2.nc)
   ncap2 -O -s 'RAIN=RAINNC+RAINC;RAINSTEP=RAIN(1:,:,:)-RAIN(:'$(($ntimes -2))',:,:);' tmp2.nc tmp.nc 
-  ncks -O -x -v RAIN,RAINC,RAINNC tmp.nc ${outdir}/$(basename ${rea}).nc
-  cdo -r settime,00:00 -daymax -selvar,T2 ${outdir}/$(basename ${rea}).nc ${outdir}/$(basename ${rea}).t2x.nc
-  cdo -r settime,00:00 -daymin -selvar,T2 ${outdir}/$(basename ${rea}).nc ${outdir}/$(basename ${rea}).t2n.nc
+  ncks -O -x -v RAIN,RAINC,RAINNC tmp.nc ${outdir}/$(basename ${rea})_${dom}.nc
+  cdo -r settime,00:00 -daymax -selvar,T2 ${outdir}/$(basename ${rea})_${dom}.nc ${outdir}/$(basename ${rea})_${dom}.t2x.nc
+  cdo -r settime,00:00 -daymin -selvar,T2 ${outdir}/$(basename ${rea})_${dom}.nc ${outdir}/$(basename ${rea})_${dom}.t2n.nc
   cdo -r settime,00:00 -daysum \
     -settaxis,${syy}-${smm}-${sdd},5,1hour \
-    -selvar,RAINSTEP ${outdir}/$(basename ${rea}).nc ${outdir}/$(basename ${rea}).pr.nc
+    -selvar,RAINSTEP ${outdir}/$(basename ${rea})_${dom}.nc ${outdir}/$(basename ${rea})_${dom}.pr.nc
+  rm -f tmp.nc tmp2.nc
+}
+
+for rea in /oceano/gmeteo/WORK/MDM.UC/WRF/experiments/oper${sdate}/*; do
+  echo "Processing: $rea"
+  procesa ${rea} d01 grid15.cdo
+  procesa ${rea} d02 grid05.cdo
 done
 
 rm -f grid15.cdo
-rm -f tmp.nc tmp2.nc
+rm -f grid05.cdo
