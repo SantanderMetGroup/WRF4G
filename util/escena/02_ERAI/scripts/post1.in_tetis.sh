0611cb6699aab05a9cbfafcbd9afe8c44383ecc1
chus <chus@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2009-11-08 19:42:14 +0000

    escena: post1 adapted to the new wrfnc_xaj.py

--- util/escena/02_ERAI/scripts/post1.in_tetis.sh
+++ util/escena/02_ERAI/scripts/post1.in_tetis.sh
@@ -4,43 +4,22 @@ scriptdir=$(dirname $0)
 basedir=$(dirname $(dirname $0))
 source ${scriptdir}/dirs
 source env.${HOSTNAME//.*/}
-source /oceano/gmeteo/WORK/chus/wrf4g/wn/lib/bash/wrf_util.sh
-
-basedir=/vols/tetis/escena/METEO4G
 
 dom=d01
-
-rea=scn5
-datei=200501
-datef=200601
-
-rea=scn6__2005122900_2009122400
-datei=200801
-datef=200812
-
-rea=scn6__1996010100_1999122700
-datei=199801
-datef=199901
-
-rea=scn3
-datei=199501
-datef=199601
 
 test -f geo_em.${dom}.nc || cp /oceano/gmeteo/WORK/MDM.UC/WRF/domains/ESCENA-UCLM2/geo_em.${dom}.nc .
 mkdir -p $POSTDIR
 
-read exp param <<< $(echo $rea | sed -e 's/__/ /g')
-
-for yearmon in $(get_yearmons ${datei:0:4} ${datei:4:2} ${datef:0:4} ${datef:4:2}); do
-  files="${basedir}/WRF/experiments/${exp}/${rea}/output/????/wrfout_${dom}_${yearmon}"
-  echo "Processing $files"
-  test "$(\ls -1 ${files}* | wc | awk '{print $1}')" -ge 28 || continue
+for year in $(seq 1989 2008); do
   python ${WRFNCXJPY} \
-    ${files}'*.nc' \
-    ${POSTDIR}/${expname}_${yearmon}_${rea}.nc \
-    RAINC,RAINNC,T2,U10ER,V10ER \
-    geo_em.${dom}.nc \
-    >& ${expname}_${yearmon}_${rea}.log
+    --from-file filelists/ESCENA_ERAI_files.${yr} \
+    -o ${POSTDIR}/${expname}_3H_${year}_prtas.nc \
+    -v RAIN,T2 \
+    -r "1950-01-01_00:00:00" \
+    -a ${WRFNCXJATTR} \
+    -t ${WRFNCXJTBL} \
+    -g geo_em.${dom}.nc \
+    >& ${expname}_3H_${year}_prtas.log
 done
 
 rm geo_em.${dom}.nc
