3e19d37c4afcac297de8f8c4fd045cf02fc3ab74
chus <chus@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2009-11-09 19:26:46 +0000

    o Changing the post1 and post2 phases in escena

--- util/escena/02_ERAI/scripts/post1.in_tetis.sh
+++ util/escena/02_ERAI/scripts/post1.in_tetis.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 thisdir=$(pwd)
-scriptdir=$(dirname $0)
-basedir=$(dirname $(dirname $0))
+scriptdir=$( (cd `dirname $0` && echo $PWD) )
+basedir=$(dirname $scriptdir)
 source ${scriptdir}/dirs
 source env.${HOSTNAME//.*/}
 
@@ -10,9 +10,9 @@ dom=d01
 test -f geo_em.${dom}.nc || cp /oceano/gmeteo/WORK/MDM.UC/WRF/domains/ESCENA-UCLM2/geo_em.${dom}.nc .
 mkdir -p $POSTDIR
 
-for year in $(seq 1989 2008); do
+for year in 1989 1999; do
   python ${WRFNCXJPY} \
-    --from-file filelists/ESCENA_ERAI_files.${yr} \
+    --from-file ${scriptdir}/filelists/ESCENA_ERAI_files.${year} \
     -o ${POSTDIR}/${expname}_3H_${year}_prtas.nc \
     -v RAIN,T2 \
     -r "1950-01-01_00:00:00" \
@@ -21,5 +21,25 @@ for year in $(seq 1989 2008); do
     -g geo_em.${dom}.nc \
     >& ${expname}_3H_${year}_prtas.log
 done
+
+python ${WRFNCXJPY} \
+  $(head -1 ${scriptdir}/filelists/ESCENA_ERAI_files.1989) \
+  -o ${POSTDIR}/${expname}_FIX_sftlf.nc \
+  -v LANDMASK \
+  -r "1950-01-01_00:00:00" --single-record \
+  -a ${WRFNCXJATTR} \
+  -t ${WRFNCXJTBL} \
+  -g geo_em.${dom}.nc \
+  >& sftlf.log
+
+python ${WRFNCXJPY} \
+  $(head -1 ${scriptdir}/filelists/ESCENA_ERAI_files.1989) \
+  -o ${POSTDIR}/${expname}_FIX_orog.nc \
+  -v HGT \
+  -r "1950-01-01_00:00:00" --single-record \
+  -a ${WRFNCXJATTR} \
+  -t ${WRFNCXJTBL} \
+  -g geo_em.${dom}.nc \
+  >& sftlf.log
 
 rm geo_em.${dom}.nc
--- util/escena/02_ERAI/scripts/post2.in_tetis.sh
+++ util/escena/02_ERAI/scripts/post2.in_tetis.sh
@@ -5,32 +5,24 @@ basedir=$(dirname $scriptdir)
 source ${scriptdir}/dirs
 source env.${HOSTNAME//.*/}
 
-function get_nc_timerecords(){
-  ncdump -h $1 | grep UNLIMITED | tr -d -c '0-9'
-}
-
-function cdo_shifted(){
-  thedate=$(ncdump -h $1 | awk '/time:units/ {print $(NF-2), $(NF-1)}' | cut -c-13 | awk '{printf "%s %d hours\n", $1,$2+17}')
-  date +'%Y-%m-%d,%H,3hour' -d "${thedate}"
-}
+mkdir -p ${POST2DIR}
 
 function procesa(){
   yr=$1
-  ncrcat -O ${POSTDIR}/${expname}_${yr}*.nc tmp.nc
-  cdo settaxis,${yr}-01-01,00:00,3hours tmp.nc tmp2.nc
-  ntimes=$(get_nc_timerecords tmp2.nc)
-  ncap2 -O -s 'RAIN=RAINNC+RAINC;RAINSTEP=RAIN(1:,:,:)-RAIN(:'$(($ntimes -2))',:,:);' tmp2.nc tmp.nc 
-  bname=${expname}__${yr}_3H
-  bnamed=${expname}__${yr}_DM
-  ncks -O -x -v RAIN,RAINC,RAINNC tmp.nc ${POST2DIR}/${bname}.nc
-  cdo -r settime,00:00 -chname,T2,tasmax -daymax -selvar,T2 ${POST2DIR}/${bname}.nc ${POST2DIR}/${bnamed}__tasmax.nc
-  cdo -r settime,00:00 -chname,T2,tasmin -daymin -selvar,T2 ${POST2DIR}/${bname}.nc ${POST2DIR}/${bnamed}__tasmin.nc
-  cdo -r settime,00:00 -chname,RAINSTEP,pr -daysum -setrtomiss,1e30,1e40 -setrtoc,-1e8,0,0 \
-    -settaxis,$(cdo_shifted ${POST2DIR}/${bname}.nc) \
-    -selvar,RAINSTEP ${POST2DIR}/${bname}.nc ${POST2DIR}/${bnamed}__pr.nc
-  rm -f tmp.nc tmp2.nc
+  inname="${POSTDIR}/${expname}_3H_${yr}_prtas.nc"
+  bnamed=${expname}_DM_${yr}
+  cdo -r settime,00:00 -chname,tas,tasmax -daymax -selvar,tas ${inname} ${POST2DIR}/${bnamed}_tasmax.nc
+  cdo -r settime,00:00 -chname,tas,tasmin -daymin -selvar,tas ${inname} ${POST2DIR}/${bnamed}_tasmin.nc
+  cdo -r settime,00:00                    -daysum -selvar,pr  ${inname} ${POST2DIR}/${bnamed}_pr.nc
 }
 
-for year in $(seq 1999 1999); do
+for year in $(seq 1989 2008); do
   procesa $year
+done
+
+cd ${POST2DIR}
+for var in pr tasmax tasmin; do
+  cdo cat ${expname}_DM_????_${var}.nc ${expname}_DM_1989-2008_${var}.nc
+  # Monthly Dic'89-Nov'08
+  cdo -setday,15 -monmean -seldate,1989-12-01T00:00,2008-11-30T23:59 ${expname}_DM_1989-2008_${var}.nc ${expname}_MM_1989-2008_${var}.nc
 done
