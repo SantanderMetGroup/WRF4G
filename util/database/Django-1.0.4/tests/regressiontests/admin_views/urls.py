f4342a25659754fd8ef6bb6d880cfc19a6997338
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2010-07-13 08:16:16 +0000

    eliminacion Django

--- util/database/Django-1.0.4/tests/regressiontests/admin_views/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-import views
-
-urlpatterns = patterns('',
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/secure-view/$', views.secure_view),
-    (r'^admin/(.*)', admin.site.root),
-)
