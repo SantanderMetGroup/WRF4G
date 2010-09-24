0891d1716598a42ed5cd3501c6c5ef32a66e6982
lluis <lluis@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2010-09-24 11:25:01 +0000

    Script to run any postprocessor after WRF4G

---
+++ ui/scripts/wrf4g_run_postprocessor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+if test $1 = '-h'
+then
+  echo "************************"
+  echo "*** Shell to run any ***"
+  echo "***  postprocessor   ***"
+  echo "***   after WRF4G    ***"
+  echo "************************"
+  echo "wrf4g_run_postprocessor.sh 'WRF4Ghome' 'postprocessor'(only name) 'DIR'(folder with wrfouts)"
+else
+  rootsh=`pwd`
+  source $1/wn/lib/bash/wrf_util.sh
+  export PATH=${rootsh}/$3/bin:${PATH}
+  export LD_LIBRARY_PATH=${rootsh}/$3/shared_libs:${LD_LIBRARY_PATH}
+
+  cd $3
+  wrfiles=`ls -1 wrfout*`
+  for wrfile in ${wrfiles} 
+  do
+    cat << EOF > ../../rootdir
+.
+EOF
+    if test ! -f $1/wn/bin/postprocessor.$2
+    then
+      echo "ERROR -- error -- ERROR -- error :'"$1"/wn/bin/postprocessor."$2"' does not exist!"
+    else
+      $1/wn/bin/postprocessor.$2 ${wrfile}
+    fi
+#    exit
+### End of wrfiles
+  done
+  dirapps=`cat ../../rootdir`
+  rm -rf ${dirapps}/bin/*
+  rm -rf ${dirapps}/lib/*
+  rmdir ${dirapps}/bin
+  rmdir ${dirapps}/lib
+  rmdir ${dirapps}
+  rm ../../rootdir  
+
+fi
