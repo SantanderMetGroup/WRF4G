f04fd14b3b9df1e42d0e52a8735387f7bbf641bd
valva <valva@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2011-04-12 12:39:57 +0000

    Last changes in new package for WRF4G

--- util/python_classes/WRF4G/Element.py
+++ util/python_classes/WRF4G/Element.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python
 
 from sys import stderr,exit, path
-path.append("/home/valva/WRF4G/util/database") 
 import vdb
 import os
-import rlcompleter
-import readline
 from re import search
 from optparse import OptionParser
-readline.parse_and_bind("tab: complete")
 
 
 
-class Wrf4gElement:
-    """  Wrf4gElement CLASS
+
+class Element:
+    """  Element CLASS
     """
  
-    def __init__(self,data,verbose='no'):
+    def __init__(self,data='',verbose='no'):
         self.verbose=verbose
         self.element=self.__class__.__name__
-        self.element='exp'
         self.data=data
         self.allfields=['','','','']        
         #for field in data.keys():            
@@ -69,7 +65,7 @@ class Wrf4gElement:
          wheresta="%s AND %s='%s'" %(wheresta,field,self.data[field])
      wheresta=wheresta[4:]     
      #dic=dbc.select(self.element,list2fields(fields), wheresta, verbose=1 )
-     dic=dbc.select(self.element,list2fields(fields),wheresta, verbose=1 )
+     dic=dbc.select(self.element,WRF4G.utils.list2fields(fields),wheresta, verbose=1 )
      self.__init__(dic[0])
      print self.sdate
      if id>0: return id
@@ -90,57 +86,7 @@ class Wrf4gElement:
        
 
     
-def pairs2dict(pairs):
-    d={}
-    for p in pairs.split(','):
-        s=p.split('=')
-        d[s[0]]=s[1]
-    return d
 
-def list2fields(arr):
-    fields=''
-    for i in arr:
-       fields=",%s,%s" %(fields,i)
-       fields=fields[2:]
-    return fields
-
-
-"""def dict2pairs(dicti):
-    fi=''
-    for field,value in dicti.items():        
-        fi= ",%s=%s"%(fi, field)
-    return fi[1:]
-"""    
-
-if __name__ == "__main__":
-    usage="""%prog [OPTIONS] exp_values function fvalues 
-             Example: %prog 
-    """
-        
-    parser = OptionParser(usage,version="%prog 1.0")
-    parser.add_option("-v", "--verbose",action="store_true", dest="verbose", default=False,help="Verbose mode. Explain what is being done")
-     
-    (options, args) = parser.parse_args()
-    
-    if len(args) < 1:
-        parser.error("Incorrect number of arguments")
-        exit(1)
-      
-    function=args[0]
-
-    if len(args) > 1:   exp_values=args[1]
-    elif len(args) > 2:   fvalues=args[2:]
-        
-    if len(args) == 1 and function == "get_all_fields":
-        exp=Wrf4gElement(data='',verbose=options.verbose)
-        output=getattr(exp,function)()
-        print ','.join(output)
-
-        
-    else:
-        exp=Wrf4gElement(pairs2dict(exp_values),options.verbose)
-        output=getattr(exp,function)(fvalues)   
-        print output
 
 
 
--- util/python_classes/WRF4G/exp.py
+++ util/python_classes/WRF4G/exp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 from sys import stderr,exit, path
-path.append("/home/valva/WRF4G/util/database") 
-import vdb
+path.append('/home/valva/WRF4G/util/python_classes')
+import WRF4G.Element
+import WRF4G.utils
 import os
 import rlcompleter
 import readline
@@ -11,99 +12,7 @@ from optparse import OptionParser
 readline.parse_and_bind("tab: complete")
 
 
-class Wrf4gElement:
-    """  Wrf4gElement CLASS
-    """
- 
-    def __init__(self,data,verbose='no'):
-        self.verbose=verbose
-        self.element=self.__class__.__name__
-        self.element='exp'
-        self.data=data
-        self.allfields=['','','','']        
-        #for field in data.keys():            
-        #    setattr(self,field,data[field])
-            
-    def get_all_fields(self):
-        dbc=vdb.vdb()
-        salida=dbc.describe(self.element)
-        return salida
-    
-    def get_id(self,fields):
-        """    
-        Query database to check if the experiment exists
-        Returns:
-        -1 --> not exists
-        exp.id --> exists with the same parameters 
-        """
-        wheresta=''
-        dbc=vdb.vdb()
-        for field in fields:
-            wheresta="%s AND %s='%s'" %(wheresta,field,self.data[field])
-        wheresta=wheresta[4:]
-        
-        idp=dbc.select(self.element,'id',wheresta,verbose=self.verbose)
-        id = vdb.list_query().one_field(idp)
-        if id !='': return id
-        else: return -1
-    
-    def get_inmutable_fields(self):
-        pass
-    
-    def get_reconfigure_fields(self):
-        pass
-    
-    def loadfromDB(self,fields):
-     """    
-     Given an array with the fields to check in the query, this function loads into 
-     self.data the Wrf4gElement values.
-     Returns:
-     0-->OK
-     1-->ERROR
-     """
-
-     wheresta=''
-     dbc=vdb.vdb()
-     for field in fields:
-         wheresta="%s AND %s='%s'" %(wheresta,field,self.data[field])
-     wheresta=wheresta[4:]     
-     #dic=dbc.select(self.element,list2fields(fields), wheresta, verbose=1 )
-     dic=dbc.select(self.element,list2fields(fields),wheresta, verbose=1 )
-     self.__init__(dic[0])
-     print self.sdate
-     if id>0: return id
-     else: return -1
-      
-    def create(self):
-
-        """
-        Create experiment
-        Returns
-        0--> Creation Worked.
-        -1--> Creation Failed
-        """
-        dbc=vdb.vdb()
-        id=dbc.insert(self.element,self.data,verbose=self.verbose)
-        if id>0: return id
-        else: return -1
-      
-    
-def pairs2dict(pairs):
-    d={}
-    for p in pairs.split(','):
-        s=p.split('=')
-        d[s[0]]=s[1]
-    return d
-
-def list2fields(arr):
-    fields=''
-    for i in arr:
-       fields=",%s,%s" %(fields,i)
-       fields=fields[2:]
-    return fields
-
-
-class exp(Wrf4gElement):
+class exp(WRF4G.Element.Element):
     
     """  Wrf4gElement CLASS
     """
@@ -134,11 +43,10 @@ if __name__ == "__main__":
         exp=exp(data='',verbose=options.verbose)
         output=getattr(exp,function)()
         print ','.join(output)
-        exp.print_hola()
 
         
     else:
-        exp=exp(pairs2dict(exp_values),options.verbose)
+        exp=exp(database.utils.pairs2dict(exp_values),options.verbose)
         output=getattr(exp,function)(fvalues)   
         print output
 
