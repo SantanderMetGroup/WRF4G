1c10f0edf72a70ccef461833127e9c4405b45186
valva <valva@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2011-06-27 12:22:36 +0000

    Added create_WRF4G_tar.sh

---
+++ create_WRF4G_tar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FOLDER="/tmp"
+echo "Creating WRF4G.tar.gz in ${FOLDER}"
+cp -RpL ui/ ${FOLDER}/
+svn info>${FOLDER}/ui/svn_info.txt
+cd ${FOLDER}/
+mv ui WRF4G
+cd WRF4G
+find ./ -name ".svn" | xargs rm -Rf
+cd ..
+tar -czvf WRF4G.tar.gz WRF4G
+
