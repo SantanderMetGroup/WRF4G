c92ab5516268d6583815c799442b52557ff34bb5
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2012-01-10 11:42:41 +0000

    Added WRF4G_LOCATION and PATH to todo.sh

--- todo.sh
+++ todo.sh
@@ -4,6 +4,8 @@ rm -rf /tmp/WRF4G*
 bash create_WRF4G_tar.sh
 cd /tmp
 tar -xzvf WRF4G.tar.gz
+export WRF4G_LOCATION=/tmp/WRF4G
+export PATH=$WRF4G_LOCATION/bin:$PATH
 wrf4g_framework start
 cd WRF4G/experiments/single_test
 sleep 10
