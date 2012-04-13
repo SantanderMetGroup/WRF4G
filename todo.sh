1579845da27261cc86d55c3dbcc947158be04615
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2012-04-13 07:41:23 +0000

    adding LD_LIBRARY_PATH and PYTHONPATH variables

--- todo.sh
+++ todo.sh
@@ -6,6 +6,8 @@ cd /tmp
 tar -xzvf WRF4G.tar.gz
 export WRF4G_LOCATION=/tmp/WRF4G
 export PATH=$WRF4G_LOCATION/bin:$PATH
+export LD_LIBRARY_PATH=$WRF4G_LOCATION/lib/shared_libs:$LD_LIBRARY_PATH
+export PYTHONPATH=$WRF4G_LOCATION/lib/python:$PYTHONPATH 
 wrf4g_framework start
 cd WRF4G/experiments/single_test
 sleep 10
