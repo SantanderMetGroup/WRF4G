3a21f67dcfe0aa5b2fc2e47931126aaa8715761a
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2012-11-18 00:54:46 +0000

    updating job template

--- ui/opt/drm4g_gridway/libexec/drm4g/managers/neptuno.py
+++ ui/opt/drm4g_gridway/libexec/drm4g/managers/neptuno.py
@@ -9,9 +9,9 @@ __revision__ = "$Id$"
 
 # The programs needed by these utilities. If they are not in a location
 # accessible by PATH, specify their location here.
-SBATCH = 'sbatch'   #submit a job
-SQUEUE = 'squeue'   #show status of jobs
-QDEL   = 'qdel'     #delete a job
+SBATCH  = 'sbatch'   #submit a job
+SQUEUE  = 'squeue'   #show status of jobs
+SCANCEL = 'scancel'  #delete a job
 
 class Resource (drm4g.managers.Resource):
 
@@ -31,7 +31,7 @@ class Resource (drm4g.managers.Resource):
         queue.Name         = 'default'
         queue.Nodes        = self.TotalCpu
         queue.FreeNodes    = self.FreeCpu
-        queue.DispatchType = 'batch'
+        queue.DispatchType = 'batch' 
         return [queue]
 
 class Job (drm4g.managers.Job):
@@ -57,40 +57,34 @@ class Job (drm4g.managers.Job):
             raise drm4g.managers.JobException(' '.join(err.split('\n')))        
 
     def jobStatus(self):
-        out, err = self.Communicator.execCommand('%s -h -o %T -j' % (SQUEUE, self.JobId))
+        out, err = self.Communicator.execCommand('squeue -h -o %T -j ' + self.JobId)
         if err:
             return 'UNKNOWN'
         elif not out:
             return 'DONE'
         else:
-            out_parser = xml.dom.minidom.parseString(out)
-            state = out.split()[3]
-            return self.states_mn.setdefault(state, 'UNKNOWN')
+            return self.states_NEPTUNO.setdefault(out.rstrip('\n'), 'UNKNOWN')
     
     def jobCancel(self):
-        out, err = self.Communicator.execCommand('%s %s' % (QDEL, self.JobId))
+        out, err = self.Communicator.execCommand('%s %s' % (SCANCEL, self.JobId))
         if err: 
             raise drm4g.managers.JobException(' '.join(err.split('\n')))
 
     def jobTemplate(self, parameters):
         args  = '#!/bin/bash\n'
-        args += '#MOAB -N JID_%s\n' % (parameters['environment']['GW_JOB_ID'])
-        args += '#MOAB -o $stdout\n'
-        args += '#MOAB -e $stderr\n'
+        args += '#SBATCH --job-name=JID_%s\n' % (parameters['environment']['GW_JOB_ID'])
+        args += '#SBATCH --output=$stdout\n'
+        args += '#SBATCH --error=$stderr\n'
         if parameters.has_key('maxWallTime'): 
-            args += '#MOAB -l walltime=%s\n' % (sec_to_H_M_S(parameters['maxWallTime']))
+            args += '#SBATCH --time=%s\n' % (sec_to_H_M_S(parameters['maxWallTime']))
         if parameters.has_key('maxMemory'):
-            args += '#MOAB -l pmem=%sMB\n' % (parameters['maxMemory'])
-        if parameters.has_key('tasksPerNode'):
-            args += '#MOAB -l nodes=%d:ppn=$tasksPerNode\n' % (int(parameters['count']) / int(parameters['tasksPerNode']))
-        else:
-            args += '#MOAB -l nodes=$count\n'
-        args += '#MOAB -v %s\n' % (','.join(['%s=%s' %(k, v) for k, v in parameters['environment'].items()]))
+            args += '#SBATCH --mem=%s\n' % (parameters['maxMemory'])
+        if parameters.has_key('tasksPerNode'): 
+            args += '#SBATCH --ntasks-per-node=$tasksPerNode'
+        args += '#SBATCH --ntasks=$count\n'
+        args += ''.join(['export %s=%s\n' % (k, v) for k, v in parameters['environment'].items()])
         args += 'cd $directory\n'
-        if parameters['jobType'] == "mpi":
-            args += 'mpiexec -np $count $executable\n'
-        else:
-            args += '$executable\n'
+        args += '$executable\n'
         return Template(args).safe_substitute(parameters)
 
 
