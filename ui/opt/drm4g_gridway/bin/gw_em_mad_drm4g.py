bff17a079131f2ff6a26c1ac6121050ef6b9bdf5
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2012-12-20 17:22:32 +0000

    there only will be one logger configuration file

--- ui/opt/drm4g_gridway/bin/gw_em_mad_drm4g.py
+++ ui/opt/drm4g_gridway/bin/gw_em_mad_drm4g.py
@@ -5,7 +5,6 @@ import os
 import traceback
 try:
     GW_LOCATION = os.environ['GW_LOCATION']
-    WRF4G_LOCATION = os.environ['WRF4G_LOCATION']
 except Exception, e:
     print 'Caught exception: %s: %s' % (e.__class__, str(e))
     sys.exit(-1)
@@ -14,13 +13,11 @@ if sys.version_info < (2,4) and sys.version_info > (3,0):
     sys.exit(-1)
 try:
     sys.path.insert(0, os.path.join(GW_LOCATION, 'libexec'))
-    sys.path.insert(0, os.path.join(WRF4G_LOCATION, 'lib','python'))
 except Exception, e:
     print 'Caught exception: %s: %s' % (e.__class__, str(e))
     traceback.print_exc(file=sys.stdout)
     sys.exit(-1)
 from drm4g.core.em_mad import GwEmMad
-import logging_wrf4g
 from optparse import OptionParser
 import exceptions
 
@@ -29,10 +26,6 @@ def main():
             prog = 'gw_em_mad_drm4g.py', version = '0.1',
             usage = 'Usage: %prog')
     options, args = parser.parse_args()
-    try:
-        logging_wrf4g.Logger(os.path.join(WRF4G_LOCATION, 'var','drm4g','em_drm4g.ini'))
-    except :
-        pass
     try:
         GwEmMad().processLine()
     except exceptions.KeyboardInterrupt, e:
