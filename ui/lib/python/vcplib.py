0a81c2e6f10d1e71aad287f15e5cf0d528394bb4
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-03-26 18:57:11 +0000

    moving vcp_matric variable

--- ui/lib/python/vcplib.py
+++ ui/lib/python/vcplib.py
@@ -268,6 +268,53 @@ class wrffile :
     It recieves a file name with one of the following shapes: wrfrst_d01_1991-01-01_12:00:00 or
     wrfrst_d01_19910101T120000Z and it return the date of the file, the name,...
     """
+    
+    def __init__(self, url, edate=None):
+        """
+        Change the name of the file in the repository (Change date to the iso format
+        and add .nc at the end of the name
+        """ 
+        # wrfrst_d01_1991-01-01_12:00:00
+        if edate:
+            self.edate = datewrf2datetime(edate)
+        
+        g = search("(.*)(\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2})", url)
+        if g:
+            base_file, date_file = g.groups() 
+            self.date = datewrf2datetime(date_file)
+        else :
+            # wrfrst_d01_19910101T120000Z.nc
+            g = search("(.*)(\d{8}T\d{6}Z)", url)
+            if not g:
+                out="File name is not well formed"
+                logging.getLogger(__name__).error(out)
+                raise Exception(out)
+            else :
+                base_file, date_file = g.groups()
+                self.date = dateiso2datetime(date_file)
+        self.file_name = basename(base_file)
+        self.dir_name = dirname(base_file)
+        
+    def date_wrf(self):
+        return datetime2datewrf(self.date)
+    
+    def date_iso(self):
+        return datetime2dateiso(self.date)
+    
+    def file_name_wrf(self):
+        return self.file_name + datetime2datewrf(self.date)
+    
+    def file_name_iso(self):
+        return "%s%s.nc" % (self.file_name,datetime2dateiso(self.date))
+    
+    def file_name_out_iso(self):
+        return "%s%s_%s.nc" % (self.file_name, datetime2dateiso(self.date), datetime2dateiso(self.edate))
+
+def copy_file(origin, destination, verbose=False, recursive=False, streams=False):
+    """
+    Copies origin in destination. Both are arrays containing the following field 
+    [protocol,user,computer,port,file]
+    """
     vcp_matrix = {'file': {'file':   {'verbose'  : '-v', 
                                   'recursive': '-R', 
                                   'command'  : "'cp %(verbose)s %(recursive)s %(orig)s %(dest)s' %param", 
@@ -321,52 +368,6 @@ class wrffile :
                        },
               }
     
-    def __init__(self, url, edate=None):
-        """
-        Change the name of the file in the repository (Change date to the iso format
-        and add .nc at the end of the name
-        """ 
-        # wrfrst_d01_1991-01-01_12:00:00
-        if edate:
-            self.edate = datewrf2datetime(edate)
-        
-        g = search("(.*)(\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2})", url)
-        if g:
-            base_file, date_file = g.groups() 
-            self.date = datewrf2datetime(date_file)
-        else :
-            # wrfrst_d01_19910101T120000Z.nc
-            g = search("(.*)(\d{8}T\d{6}Z)", url)
-            if not g:
-                out="File name is not well formed"
-                logging.getLogger(__name__).error(out)
-                raise Exception(out)
-            else :
-                base_file, date_file = g.groups()
-                self.date = dateiso2datetime(date_file)
-        self.file_name = basename(base_file)
-        self.dir_name = dirname(base_file)
-        
-    def date_wrf(self):
-        return datetime2datewrf(self.date)
-    
-    def date_iso(self):
-        return datetime2dateiso(self.date)
-    
-    def file_name_wrf(self):
-        return self.file_name + datetime2datewrf(self.date)
-    
-    def file_name_iso(self):
-        return "%s%s.nc" % (self.file_name,datetime2dateiso(self.date))
-    
-    def file_name_out_iso(self):
-        return "%s%s_%s.nc" % (self.file_name, datetime2dateiso(self.date), datetime2dateiso(self.edate))
-
-def copy_file(origin, destination, verbose=False, recursive=False, streams=False):
-    """
-    Copies origin in destination. Both are arrays containing the following field 
-    [protocol,user,computer,port,file]
-    """
     logger = logging.getLogger(__name__)
     out="Starting to copy ..."
     logger.debug(out)
@@ -397,7 +398,7 @@ def copy_file(origin, destination, verbose=False, recursive=False, streams=False
     logger.debug(out)   
     if verbose :
         stderr.write(out + "\n")        
-    param = self.vcp_matrix[orig.protocol][dest.protocol]
+    param = vcp_matrix[orig.protocol][dest.protocol]
     orig_file = eval(param['orig'])
     dest_file = eval(param['dest'])
     
