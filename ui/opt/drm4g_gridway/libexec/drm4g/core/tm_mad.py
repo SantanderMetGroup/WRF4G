e7ae0c25c9adb5d465a63004bc653badf71d1a2b
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-03-27 17:56:57 +0000

    checking ".lock" file

--- ui/opt/drm4g_gridway/libexec/drm4g/core/tm_mad.py
+++ ui/opt/drm4g_gridway/libexec/drm4g/core/tm_mad.py
@@ -140,8 +140,9 @@ class GwTmMad (object):
         """
         OPERATION, JID, TID, EXE_MODE, SRC_URL, DST_URL = args.split()
         try:
-            com = self._updateCom(urlparse(SRC_URL).host)            
-            com.rmDirectory(SRC_URL)
+            com = self._updateCom(urlparse(SRC_URL).host)
+            if not com.checkOutLock(SRC_URL):
+                com.rmDirectory(SRC_URL)
             out = 'RMDIR %s - SUCCESS -' % (JID)
         except Exception, e:
             out = 'RMDIR %s - FAILURE %s' % (JID, str(e))
