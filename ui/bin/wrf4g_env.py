d68c9a5b437ed25e0a703a30d86c9dcd1aaa16a0
antonio <antonio@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-04-06 22:18:14 +0000

    Including Shell and Python environement loading scripts

---
+++ ui/bin/wrf4g_env.py
@@ -0,0 +1,21 @@
+import os,sys
+
+WRF4G_LOCATION=os.environ.get('WRF4G_LOCATION')
+if WRF4G_LOCATION == None:
+    WRF4G_LOCATION=os.path.dirname(os.path.dirname(__file__))
+    os.environ['WRF4G_LOCATION']=WRF4G_LOCATION
+
+os.environ['DB4G_CONF']='%s/etc/db4g.conf'%WRF4G_LOCATION
+DB4G_CONF=os.environ['DB4G_CONF']
+if not os.path.isfile(DB4G_CONF):
+    print 'DB4G_CONF do not exist'
+    exit(2)
+
+sys.path.insert(0, os.path.join(WRF4G_LOCATION, 'lib', 'python'))
+
+from sys import stderr,exit, path
+from optparse import OptionParser
+import WRF4G,exceptions,re,traceback,logging.config
+import vcplib,vdblib
+
+logging.config.fileConfig(os.path.join(WRF4G_LOCATION,'etc','logger.conf'))
