7124262f6d429dc9000e0495de7c979f4d3f9384
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-04-16 14:11:47 +0000

    adding drm4g_env file in order to configure drm4g commands

--- ui/opt/drm4g_gridway/bin/gw_em_mad_drm4g.py
+++ ui/opt/drm4g_gridway/bin/gw_em_mad_drm4g.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import traceback
-import logging.config
-from os.path import dirname, join
-
-if sys.version_info < (2,4) and sys.version_info > (3,0):
-    print 'The version number of the Python has to be > = 2.4 and < 3.0'
-    sys.exit(-1)
-try:
-    sys.path.insert(0, join(dirname(dirname(os.path.abspath(__file__))), 'libexec'))
-    from drm4g.global_settings import PATH_LOGGER
-    try:
-        logging.config.fileConfig(PATH_LOGGER)
-    except :
-        pass
-except Exception, e:
-    print 'Caught exception: %s: %s' % (e.__class__, str(e))
-    traceback.print_exc(file=sys.stdout)
-    sys.exit(-1)
+from drm4g_env import *
 from drm4g.core.em_mad import GwEmMad
-from optparse import OptionParser
-import exceptions
 
 def main():
     parser = OptionParser(description = 'Execution manager MAD',
