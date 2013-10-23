f0ed7652a592f22da4822dd1e8812d45a0c07694
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-10-23 11:17:12 +0000

    deleting drm4g_gridway directory

--- opt/drm4g_gridway/libexec/drm4g/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__all__ = ["communicators", "core", "handlers", "utils"]
-
-__version__ = '0.1'
-__author__  = 'Carlos Blanco'
-__revision__ = "$Id: __init__.py 1744 2013-02-06 12:03:36Z carlos $"
