1f2395ebbb206cefa269c198c56e7babbf0504f5
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2014-04-24 10:58:37 +0000

    Enabling auto-complete support in the wrf4g interpreter

--- lib/python/wrf4g/__init__.py
+++ lib/python/wrf4g/__init__.py
@@ -16,7 +16,8 @@ GW_BIN_LOCATION           = join( WRF4G_DEPLOYMENT_LOCATION , 'opt' , 'gw_drm4g'
 GW_LIB_LOCATION           = join( WRF4G_DEPLOYMENT_LOCATION , 'opt' , 'gw_drm4g' , 'libexec' )
 MYSQL_LOCATION            = join( WRF4G_DEPLOYMENT_LOCATION , 'opt' , 'mysql-5.5' )
 
-if exists( WRF4G_LOCATION ) is False and WRF4G_LOCATION is not "NONE" :
+if exists( WRF4G_LOCATION ) is False and WRF4G_LOCATION != "NONE" :
+    print "Creating a WRF4G local configuration in '%s'" % WRF4G_LOCATION
     directories  = [
                     'var/log' , 
                     'var/submission' , 
@@ -24,17 +25,37 @@ if exists( WRF4G_LOCATION ) is False and WRF4G_LOCATION is not "NONE" :
                     'opt/gw_drm4g/var' ,
                     ]
     for dir in directories :
-        os.makedirs( join ( WRF4G_LOCATION , dir ) )
+        abs_dir = join ( WRF4G_LOCATION , dir )
+        print "Creating '%s' directory" % abs_dir
+        os.makedirs( abs_dir )
     from  shutil import copytree
     for dir_to_copy in [ 'etc' , 'opt/gw_drm4g/etc' , 'repository' ] :
-        copytree( 
-                 join ( WRF4G_DEPLOYMENT_LOCATION  , dir_to_copy ) ,
-                 join ( WRF4G_LOCATION             , dir_to_copy )
-                 )    
+        src  = join ( WRF4G_DEPLOYMENT_LOCATION  , dir_to_copy )
+        dest = join ( WRF4G_LOCATION             , dir_to_copy )
+        print "Coping from '%s' to '%s'" % ( src , dest )
+        copytree( src , dest )    
 try :
     import logging.config  
     logging.config.fileConfig( 
                               join( WRF4G_LOCATION , 'etc' , 'logger.conf') , 
                               {'WRF4G_LOCATION' : WRF4G_LOCATION } 
                                )
-except : pass
+except : 
+    pass
+
+if WRF4G_LOCATION != "NONE" : 
+    import readline
+    import rlcompleter
+    import atexit
+    # tab completion
+    readline.parse_and_bind('tab: complete')
+    # history file
+    histfile = join(  WRF4G_LOCATION , '.wrf4ghistory' )
+    try:
+        readline.read_history_file( histfile )
+    except IOError:
+        pass
+    atexit.register(readline.write_history_file, histfile)
+    del histfile, readline, rlcompleter
+
+
