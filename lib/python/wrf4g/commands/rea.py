2e9c7cef12851b608f819a5dba9ef63e14183c25
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2015-04-06 12:09:10 +0000

    adding a command to manage WRF4G realizations

---
+++ lib/python/wrf4g/commands/rea.py
@@ -0,0 +1,61 @@
+"""
+Manage realizations. 
+    
+Usage: 
+    wrf4g rea <name> submit [ <fist_ch> <last_ch> ] [ --dbg ] [ --rerun ] [ --dry-run ]
+    wrf4g rea <name> status [ --dbg ] [ --long ] 
+    wrf4g rea <name> stop   [ --dbg ] [ --dry-run ] 
+   
+Options:
+   --dbg                Debug mode.
+   -n --dry-run         Dry run.
+   -l --long            Show a detailed information.
+   --rerun              Force to run although this realization or experiment has finished.
+  
+Commands:
+   submit
+   status
+   stop
+"""
+__version__  = '2.0.0'
+__author__   = 'Carlos Blanco'
+__revision__ = "$Id$"
+
+import logging
+from sqlalchemy.orm.exc   import NoResultFound
+from wrf4g                import logger
+from wrf4g.db             import get_session
+from wrf4g.core           import Realization
+
+def run( arg ) :
+    if arg[ '--dbg' ] :
+        logger.setLevel( logging.DEBUG )
+    # create a session to connect with the database
+    session = get_session()
+    try :
+        try : 
+            q_rea = session.query( Realization ).\
+                    filter( Realization.name == arg[ '<name>' ] ).one()
+        except NoResultFound :
+            raise Exception( "'%s' realization does not exist" % arg[ '<name>' ] )
+        else :
+            rea = q_rea()
+            rea.session = session
+            if arg[ 'submit' ] :
+                rea.run( first_chunk_run = arg[ '<first_ch>' ], 
+                         last_chunk_run  = arg[ '<last_ch>' ] , 
+                         rerun           = arg[ 'rerun' ], 
+                         dryrun          = arg[ '--dry-run' ] )
+            elif arg[ 'status' ] :
+                rea.status( long_format = arg[ '--long' ] )
+            else :
+                rea.stop( dryrun = arg[ '--dry-run' ] )
+            if arg[ '--dry-run' ] :
+                session.rollback()
+            else :
+                session.commit()
+    except Exception , err :
+        session.rollback()
+        logger.error( str( err ) )
+    finally:
+        session.close()
