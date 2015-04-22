9f8578afc82ba479fcdbd49f5e23b4ad876e0ac7
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2015-04-22 07:05:49 +0000

    deleting gw_drm4g directory

--- opt/gw_drm4g/libexec/drm4g/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__all__ = ["communicators", "core", "handlers", "utils"]
-
-__version__  = '2.2.0'
-__author__   = 'Carlos Blanco'
-__revision__ = "$Id: __init__.py 2250 2014-08-27 09:04:57Z carlos $"
