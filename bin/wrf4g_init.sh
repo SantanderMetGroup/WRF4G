4c104d8c3d5f73773ecf319c3908639ed248bce6
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2015-07-27 11:33:58 +0000

    adding WRF4G_DEPLOYMENT_DIR variable

--- bin/wrf4g_init.sh
+++ bin/wrf4g_init.sh
@@ -1,6 +1,6 @@
-WRF4G_BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-WRF4G_DIR=$( dirname $WRF4G_BIN )
-if [[ ":$PATH:" != *":$WRF4G_BIN:"* ]]; then export PATH=$WRF4G_BIN:${PATH}; fi
-if [[ ":$PYTHONPATH:" != *":$WRF4G_DIR/lib/python:"* ]]; then export PYTHONPATH=$WRF4G_DIR/lib/python:${PYTHONPATH}; fi
-if [[ ":$PYTHONPATH:" != *":$WRF4G_DIR/opt/drm4g/libexec:"* ]]; then export PYTHONPATH=$WRF4G_DIR/opt/drm4g/libexec:${PYTHONPATH}; fi
-source ${WRF4G_BIN}/wrf4g_autocomplete.sh
+WRF4G_DEPLOYMENT_DIR_BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+WRF4G_DEPLOYMENT_DIR=$( dirname ${WRF4G_DEPLOYMENT_DIR_BIN} )
+if [[ ":${PATH}:" != *":${WRF4G_DEPLOYMENT_DIR}:"* ]]; then export PATH=${WRF4G_DEPLOYMENT_DIR_BIN}:${PATH}; fi
+if [[ ":${PYTHONPATH}:" != *":${WRF4G_DEPLOYMENT_DIR}/lib/python:"* ]]; then export PYTHONPATH=${WRF4G_DEPLOYMENT_DIR}/lib/python:${PYTHONPATH}; fi
+if [[ ":${PYTHONPATH}:" != *":${WRF4G_DEPLOYMENT_DIR}/opt/drm4g/libexec:"* ]]; then export PYTHONPATH=${WRF4G_DEPLOYMENT_DIR}/opt/drm4g/libexec:${PYTHONPATH}; fi
+source ${WRF4G_DEPLOYMENT_DIR_BIN}/wrf4g_autocomplete.sh
