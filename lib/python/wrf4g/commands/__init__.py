35a0246f4bd0f372d5a78e598e3874a78cc4227d
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2016-06-06 16:33:54 +0000

    Command name auto-correct.

---
+++ lib/python/wrf4g/commands/__init__.py
@@ -0,0 +1,15 @@
+__version__  = '2.2.1'
+__author__   = 'Carlos Blanco'
+__revision__ = "$Id$"
+
+def get_similar_commands( name, sub_commands ):
+    """
+    Command name auto-correct.
+    """
+    from difflib import get_close_matches
+    name = name.lower()
+    close_commands = get_close_matches( name, sub_commands )
+    if close_commands:
+        return close_commands[0]
+    else:
+        return None
