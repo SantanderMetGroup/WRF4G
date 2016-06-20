c9d5c525a200aa4361f9c403560b6dccbbd0c3cd
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2016-06-20 11:43:21 +0000

    using WRF4G_DIR to read local wrf4g configurations

--- lib/python/wrf4g/__init__.py
+++ lib/python/wrf4g/__init__.py
@@ -7,7 +7,7 @@ __author__   = 'Carlos Blanco'
 __revision__ = "$Id$"
 
 HOME                 = os.environ.get( 'HOME' )
-WRF4G_DIR            = os.environ[ 'GW_LOCATION' ] = os.environ.get( 'WRF4G_DIR', join ( HOME, '.wrf4g' ) )
+WRF4G_DIR            = os.environ[ 'GW_LOCATION' ] = join( os.environ.get( 'WRF4G_DIR', HOME ), '.wrf4g' )
 WRF4G_DEPLOYMENT_DIR = dirname( dirname( dirname( dirname( abspath( __file__ ) ) ) ) )
 DB4G_CONF            = os.environ.get( 'DB4G_CONF',  join( WRF4G_DIR , 'etc' , 'db.conf' ) )
 WRF4G_LOGGER         = join( WRF4G_DIR, 'etc', 'logger.conf')
