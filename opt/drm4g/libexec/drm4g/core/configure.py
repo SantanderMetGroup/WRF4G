c7b879ccb03f23e0f1f29ec037746550b4691927
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2016-06-29 20:03:51 +0000

    improving message

--- opt/drm4g/libexec/drm4g/core/configure.py
+++ opt/drm4g/libexec/drm4g/core/configure.py
@@ -62,7 +62,6 @@ class Configuration(object):
                 except Exception as err:
                     output = "Configuration file '%s' is unreadable or malformed: %s" % ( DRM4G_CONFIG_FILE , str( err ) )
                     logger.error( output )
-                    raise ConfigureException( output )
                 
                 for sectname in parser.sections():
                     name                   = sectname
@@ -73,7 +72,6 @@ class Configuration(object):
             except Exception as err:
                 output = "Error reading '%s' file: %s" % (DRM4G_CONFIG_FILE, str(err)) 
                 logger.error( output )
-                raise ConfigureException( output )
         finally:
             file.close()
             
@@ -183,7 +181,6 @@ class Configuration(object):
             except Exception as err:
                 output = "Failed creating communicator for resource '%s' : %s" % ( name, str( err ) )
                 logger.warning( output , exc_info=1 )
-                raise ConfigureException( output )
         return communicators 
 
     def make_resources(self):
@@ -207,5 +204,4 @@ class Configuration(object):
             except Exception as err:
                 output = "Failed creating objects for resource '%s' of type : %s" % ( name, str( err ) )
                 logger.warning( output , exc_info=1 )
-                raise ConfigureException( output )
         return resources
