a7d6ea8192d0a3c451209f184b163b9bde6afadd
fernanqv <fernanqv@users.noreply.github.com>
2021-03-12 23:17:35 +0100

    Changed DRM4G_DIR

--- wrf4g/__init__.py
+++ wrf4g/__init__.py
@@ -27,7 +27,8 @@ __author__   = 'Carlos Blanco'
 __revision__ = "$Id$"
 
 HOME                 = os.environ.get( 'HOME' )
-WRF4G_DIR            = os.environ[ 'GW_LOCATION' ] = join( os.environ.get( 'WRF4G_DIR', HOME ), '.wrf4g' )
+os.environ[ 'DRM4G_DIR' ] = join( os.environ.get( 'WRF4G_DIR', HOME ), '.drm4g' )
+WRF4G_DIR            = join( os.environ.get( 'WRF4G_DIR', HOME ), '.wrf4g' )
 WRF4G_DEPLOYMENT_DIR = os.path.dirname(os.path.realpath(__file__))
 DB4G_CONF            = os.environ.get( 'DB4G_CONF',  join( WRF4G_DIR , 'etc' , 'db.conf' ) )
 WRF4G_LOGGER         = join( WRF4G_DIR, 'etc', 'logger.conf')
